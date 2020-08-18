fbdd114ec79e9b7cea5cfdea57ec5874b6d27be4
Ramon Snellink <r.snellink@gmail.com>
2020-08-18 21:07:22 +0200

    started wikipedia search widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   { title: "What is React?", content: "React is a front-end Javascript framework." },
@@ -9,8 +10,8 @@ const items = [
 
 export default () => {
   return (
-    <div>   
-      <Accordion items={items} />
+    <div>
+      <Search />
     </div>
   );
 };
