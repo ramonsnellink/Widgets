c8e0aefed1b50412663e68614a260a763b6b03e1
Ramon Snellink <r.snellink@gmail.com>
2020-08-22 21:03:54 +0200

    started on dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   { title: "What is React?", content: "React is a front-end Javascript framework." },
@@ -8,10 +9,17 @@ const items = [
   { title: "How do you use React?", content: "You use React by creating components." },
 ];
 
+const options = [
+  { label: "The Color Red", value: "red" },
+  { label: "The Color Green", value: "green" },
+  { label: "A shade of Blue", value: "blue" },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Search />
+      <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  // Set up a seperate useEffect. This only runs once (thus the empty array).
+  // This will set up a SEPERATE event listener for the body, and checks if the ref is clicked or not
+  // If it is not clicked, set open to false (close the dropdown.)
+  // We do this because otherwise the menu won't close if the user clicks anywhere on the screen to close.
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      //Check if the current ref contains the item that was clicked.
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    });
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      //onSelectedChange is calling setSelected that sets the "selected" state
+      <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form" ref={ref}>
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)} //set open to the opposite of it's current value
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Dropdown;
